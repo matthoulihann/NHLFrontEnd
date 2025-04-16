c407feb3549ccf54273c2f25029da2391a8ca6e0
matthoulihann <153053833+matthoulihann@users.noreply.github.com>
2025-04-16 13:51:59 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
-export const isClient = typeof window !== "undefined"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * Check if code is running on the client side
+ * @returns true if running in a browser, false if running on the server
+ */
+export function isClient(): boolean {
+  return typeof window !== "undefined"
 }
